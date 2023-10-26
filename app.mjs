d1c34be6863e5cee7b01ee7c132daba037e69058
PythagorasDominium <pythagoras@dominiumdao.finance>
2023-10-26 14:21:24 -0700

    Push

--- app.mjs
+++ app.mjs
@@ -7,10 +7,30 @@ const API_TOKEN = '8ebgdsgiihnutdglcponabcvadj5166nmob6m8g2gnene3v0eqvb8q2385oue
 let salesRepNames = {};
 
 async function fetchEmployeeName(employeeID) {
-    const response = await fetch(`${API_URL}/employee/${employeeID}`);
-    const data = await response.json();
-    console.log("Employee data for ID " + employeeID, data);  // Logging the response
-    return `${data.fname} ${data.lname}`;
+    const headers = {
+        'Authorization': `Bearer ${API_KEY}:${API_TOKEN}`
+    };
+
+    try {
+        const response = await fetch(`${API_URL}/employee/${employeeID}`, { headers });
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log("Employee data for ID " + employeeID, data);  // Logging the response
+        return `${data.fname} ${data.lname}`;
+    } catch (error) {
+        console.error("Error fetching employee data:", error);
+        throw error;
+    }
 }
 
-fetchEmployeeName('10004');  // replace '10004' with a valid employee ID.
+fetchEmployeeName('10004')
+    .then(employeeName => {
+        console.log("Employee name:", employeeName);
+    })
+    .catch(err => {
+        console.error("Error:", err);
+    });
