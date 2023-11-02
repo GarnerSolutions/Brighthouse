609c998f32fa871df3eca7a15d86d011fa3d1269
YooTek <jurij.tekavec.development@gmail.com>
2023-11-02 22:29:49 +0100

    google calendar API

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_TOKEN = exports.API_KEY = exports.API_URL = void 0;
-exports.API_URL = 'https://brighthousesolar.fieldroutes.com/api';
-exports.API_KEY = '2l6dhlhl6tltpfu98dpvb6770a0mrro8i1tcctk66ksurks44ceohif28o30um17';
-exports.API_TOKEN = '8ebgdsgiihnutdglcponabcvadj5166nmob6m8g2gnene3v0eqvb8q2385ouedcu';
--- googleCalendar.ts
+++ googleCalendar.ts
@@ -1,47 +1,75 @@
 
 import axios from 'axios';
-import google from "googleapis"
-
+import { google } from "googleapis"
+import key from "./brighthouse-393023-f224c2e17667"
 
 // Set your Google Calendar API key.
-const apiKey = 'AIzaSyCW7vyJMLd7f_wIxBAH6MqQ8JCqTiybc7Y';
 
-//@ts-ignore
-const calendar = google.calendar_v3({
-    version: 'v3',
-    auth: apiKey,
-});
 
+const calendarId = "jurij@brighthouse.solar"
 
 async function fetchGoogleCalendarEvents() {
     try {
         // Set the calendar ID (usually 'primary' for the authenticated user's primary calendar).
-        const calendarId = 'primary';
+
 
         // Define the time range for events retrieval (optional).
         const timeMin = new Date().toISOString();
         const timeMax = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).toISOString(); // Fetch events for the next 7 days.
+        // Create a JWT client using the service account key
+        const jwtClient = new google.auth.JWT({
+            email: key.client_email,
+            key: key.private_key,
+            scopes: ['https://www.googleapis.com/auth/calendar'],
+        });
+
+        // Authenticate the client
+        jwtClient.authorize(async (err) => {
+            if (err) {
+                console.error('Error authenticating service account:', err);
+                return;
+            }
+            // Create an instance of the Google Calendar API
+            const calendar = google.calendar({ version: 'v3', auth: jwtClient });
+
+
+            const e = await calendar.events.list({
+                calendarId: calendarId,
+                timeMin: timeMin,
+                timeMax: timeMax
+            })
+
+            console.log(e.data)
+
+            const event = {
+                summary: 'Event Title',
+                location: 'Event Location',
+                description: 'Event Description',
+                start: {
+                    dateTime: new Date(new Date().getTime() + 1 * 24 * 60 * 60 * 1000).toISOString(),
+                    timeZone: 'CET',
+                },
+                end: {
+                    dateTime: new Date(new Date().getTime() + 1 * 25 * 60 * 60 * 1000).toISOString(),
+                    timeZone: 'CET',
+                },
+            };
+            const k = await calendar.events.insert({
+                calendarId: calendarId,
+                requestBody: event
+            })
+
+            console.log(k)
 
 
 
-        // Build the URL for the Google Calendar API.
-        const apiUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}&timeMin=${timeMin}&timeMax=${timeMax}`;
+
+        });
 
 
-        // Make a GET request to the Google Calendar API.
-        const response = await axios.get(apiUrl);
 
-        const events = response.data.items;
 
-        if (events.length) {
-            console.log('Upcoming events:');
-            events.forEach((event: any, i: number) => {
-                const start = event.start.dateTime || event.start.date;
-                console.log(`${i + 1}. ${start} - ${event.summary}`);
-            });
-        } else {
-            console.log('No upcoming events found.');
-        }
+
     } catch (error) {
         console.error('Error fetching events:', JSON.stringify(error));
 
